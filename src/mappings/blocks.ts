7ea9e81bafcfedbbba94ccfde6a02797e71536b5
ianlapham <ianlapham@gmail.com>
2021-12-20 19:02:34 -0500

    update for optimism

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -2,20 +2,20 @@ import { ethereum } from '@graphprotocol/graph-ts'
 import { Block } from '../types/schema'
 
 export function handleBlock(block: ethereum.Block): void {
-  let id = block.hash.toHex()
-  let blockEntity = new Block(id)
+  const id = block.hash.toHex()
+  const blockEntity = new Block(id)
   blockEntity.number = block.number
   blockEntity.timestamp = block.timestamp
   blockEntity.parentHash = block.parentHash.toHex()
-  blockEntity.author = block.author.toHex()
   blockEntity.difficulty = block.difficulty
+  blockEntity.author = block.author.toHex()
   blockEntity.totalDifficulty = block.totalDifficulty
   blockEntity.gasUsed = block.gasUsed
   blockEntity.gasLimit = block.gasLimit
   blockEntity.receiptsRoot = block.receiptsRoot.toHex()
   blockEntity.transactionsRoot = block.transactionsRoot.toHex()
   blockEntity.stateRoot = block.stateRoot.toHex()
-  blockEntity.size = block.size
   blockEntity.unclesHash = block.unclesHash.toHex()
+  blockEntity.size = block.size
   blockEntity.save()
 }
